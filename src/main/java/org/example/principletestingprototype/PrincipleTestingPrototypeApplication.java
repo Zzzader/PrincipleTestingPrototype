eab1279d3e370562bb2e1bfa6c35e1fff75f0d07
Zerrin Celik <s1152974@student.hsleiden.nl>
2026-09-11 20:12:42 +0200

    Initial commit

---
+++ src/main/java/org/example/principletestingprototype/PrincipleTestingPrototypeApplication.java
@@ -0,0 +1,13 @@
+package org.example.principletestingprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipleTestingPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrincipleTestingPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/principletestingprototype/PrincipleTestingPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.principletestingprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipleTestingPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
