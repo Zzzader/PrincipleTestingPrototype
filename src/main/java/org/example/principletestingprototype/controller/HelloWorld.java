56cde87ac50f4e40448a5b633f2b825e132e4b7c
Zerrin Celik <s1152974@student.hsleiden.nl>
2026-09-14 12:07:57 +0200

    test: testing the pipleine

--- src/main/java/org/example/principletestingprototype/controller/HelloWorld.java
+++ src/main/java/org/example/principletestingprototype/controller/HelloWorld.java
@@ -19,7 +19,7 @@ public class HelloWorld {
         String result = "Hello World";
         String input = helloWorldDTO.name;
         if (input.equals(result) ){
-            return ResponseEntity.ok("Access granted");
+            return ResponseEntity.ok("Access granted - welcome :D");
         }
         else{
             return ResponseEntity.ok("Access denied");
